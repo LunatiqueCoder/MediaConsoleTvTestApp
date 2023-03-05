dab1d5808611747a94f2e675001d974e93484ced
Ovidiu Cristescu <55203625+criszz77@users.noreply.github.com>
2023-03-05 22:12:07 +0200

    onEnd bug

--- App.tsx
+++ App.tsx
@@ -1,9 +1,19 @@
 import 'react-native/tvos-types.d';
-import React from 'react';
+import React, {useRef} from 'react';
 import VideoPlayer from 'react-native-media-console';
+import Video from 'react-native-video';
 
 const App = () => {
-  return <VideoPlayer source={{uri: 'https://vjs.zencdn.net/v/oceans.mp4'}} />;
+  const videoRef = useRef<Video>(null);
+  const onEnd = () => videoRef?.current?.seek(0); // https://github.com/react-native-video/react-native-video/issues/836#issuecomment-341381704
+
+  return (
+    <VideoPlayer
+      videoRef={videoRef}
+      source={{uri: 'https://vjs.zencdn.net/v/oceans.mp4'}}
+      onEnd={onEnd}
+    />
+  );
 };
 
 export default App;
